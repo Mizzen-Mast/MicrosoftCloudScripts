5d2921c750cea7c1f04f57cbf8118fd631ad6780
Mizzen Mast <18427302+Mizzen-Mast@users.noreply.github.com>
2024-05-07 10:50:12 -0600

    Updaed inactive users to account for shared accounts

--- inactiveUsers.ps1
+++ inactiveUsers.ps1
@@ -1,13 +1,33 @@
 #first set permissions to allow reading from audit log and Directory. 
-Connect-MgGraph -Scope AuditLog.Read.All,Directory.Read.All
+Connect-MgGraph -Scope AuditLog.Read.All,Directory.Read.All -NoWelcome
+Connect-ExchangeOnline -ShowBanner:$false
+
 
 $dateConsideredInactive = (Get-Date).AddDays(-90)
 $accountCreatedBefore = (Get-Date).AddDays(-30)
 
 #get all users that have accounts enabled and not guest accounts
-$users = Get-MgUser -All -Property DisplayName,Mail,UserPrincipalName,UserType,SignInActivity,AccountEnabled,CreatedDateTime | Where-Object { ($_.AccountEnabled -eq $true ) -and ( $_.UserType -eq 'Member' ) -and { $_.CreatedDateTime -lt $accountCreatedBefore } }
+$users = Get-MgUser -All -Property DisplayName,Mail,UserPrincipalName,UserType,SignInActivity,AccountEnabled,CreatedDateTime | Where-Object { ($_.AccountEnabled -eq $true ) -and ( $_.UserType -eq 'Member' ) -and ( $_.CreatedDateTime -lt $accountCreatedBefore ) }
 
 #filter for the last sign in date time before 90 days. 
 $inactiveUsers = $users | Where-Object { $_.SignInActivity.LastSignInDateTime -lt $dateConsideredInactive -and $_.SignInACtivity.LastNonInteractiveSignInDateTime -lt $dateConsideredInactive }
 
-$inactiveUsers
+$sharedInactive = @()
+$notSharedInactive = @()
+
+#filter for sharedmailbox. 
+ForEach($account in $inactiveUsers) { 
+
+    If ((Get-Mailbox $account.Mail).RecipientTypeDetails -eq 'SharedMailbox') { 
+
+            $sharedInactive += $account
+    }
+    Else { 
+        $notSharedInactive += $account
+    }
+
+}
+
+
+$sharedInactive | Select-Object -Property DisplayName,UserPrincipalName | Export-Csv -Path C:\Users\KyleClayson\Downloads\SharedinActive.csv -NoTypeInformation
+$NotsharedInactive | Select-Object -Property DisplayName,UserPrincipalName | Export-Csv -Path C:\Users\KyleClayson\Downloads\NotSharedinActive.csv -NoTypeInformation
